710de90ebbee589faf4b666195579ea844768346
penielpam <penielpam@gmail.com>
2023-05-14 11:42:51 +0000

    made somechanges

--- tests/test_console.py
+++ tests/test_console.py
@@ -17,7 +17,7 @@ import unittest
 from models import storage
 from models.engine.file_storage import FileStorage
 from console import HBNBCommand
-from io import 
+from io import StringIO
 from unittest.mock import patch
 
 
